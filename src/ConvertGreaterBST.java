1fdbe83f7b23e47754ffca961c8fc32c6cbe3086
panmengguan <pmengguan@gmail.com>
2017-09-06 16:58:46 +0800

    Convert Greater BST

--- src/ConvertGreaterBST.java
+++ src/ConvertGreaterBST.java
@@ -1,17 +1,37 @@
 public class ConvertGreaterBST {
+    // version 1
+//    public TreeNode convertBST(TreeNode root) {
+//        return convert(root, 0);
+//    }
+//
+//    private int findMin(TreeNode root) {
+//        if (root == null) return 0;
+//        if (root.left == null) return root.val;
+//        else return findMin(root.left);
+//    }
+//
+//    private TreeNode convert(TreeNode root, int offset) {
+//        if (root == null) return null;
+//        TreeNode root1 = new TreeNode(root.val);
+//        root1.right = convert(root.right, offset);
+//        if (root1.right != null)
+//            root1.val += findMin(root1.right);
+//        else
+//            root1.val += offset;
+//        root1.left = convert(root.left, root1.val);
+//        return root1;
+//    }
     public TreeNode convertBST(TreeNode root) {
-        if (root == null) return null;
-        return convert(root, 0);
+        dfs(root, 0);
+        return root;
     }
 
-    private TreeNode convert(TreeNode root, int offset) {
-        if (root == null) return null;
-        TreeNode root1 = new TreeNode(root.val + offset);
-        if (root.right != null) {
-            root1.right = convert(root.right, 0);
-            root1.val += root1.right.val;
-        }
-        root1.left = convert(root.left, root1.val);
-        return root1;
+    private int dfs(TreeNode root, int offset) {
+        if (root == null) return offset;
+        int right = dfs(root.right, offset);
+        root.val += right;
+        int left = dfs(root.left, root.val);
+        return left;
     }
+
 }
