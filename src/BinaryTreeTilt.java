54ff076443f37f19ee7bc30dd777590a54233de7
panmengguan <pmengguan@gmail.com>
2017-09-07 11:29:25 +0800

    Binary Tree Tilt

---
+++ src/BinaryTreeTilt.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class BinaryTreeTilt {
+    private int sum = 0;
+    public int findTilt(TreeNode root) {
+        helper(root);
+        return sum;
+    }
+
+    private int helper(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = helper(root.left);
+        int right = helper(root.right);
+        sum += Math.abs(left - right);
+        return left + right + root.val;
+    }
+}
